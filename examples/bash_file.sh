dbf66a1a65e7b45c9a33ce566979dbf818e055fc
AndreGraca98 <andre.p.g@sapo.pt>
2022-10-11 23:54:53 +0100

    Add optional environment name to bash_file

--- examples/bash_file.sh
+++ examples/bash_file.sh
@@ -1,5 +1,9 @@
+if [ $# -eq 0 ]; then
+    env_name=jobqueue
+else
+    env_name=$1
+fi
 
-
-$HOME/anaconda3/envs/jobqueue/bin/python $HOME/SharedJobsQueue/examples/random_sleep.py
+$HOME/anaconda3/envs/$env_name/bin/python $HOME/SharedJobsQueue/examples/random_sleep.py
 
 echo "finished running the bash script!"
